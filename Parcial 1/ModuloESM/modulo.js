09fa517c39cfa8e9b13b3bf3cb3331fba6df1f8e
dan260gk <90198305+dan260gk@users.noreply.github.com>
2023-08-29 10:17:15 -0500

    actualizaciones

---
+++ Parcial 1/ModuloESM/modulo.js
@@ -0,0 +1,16 @@
+
+let estado=[
+    "200 Ok",
+    "400 Bad Request",
+    "403 Forbidden",
+    "404 Not Found",
+    "408 Request Timeout",
+    "429 Too Many Request",
+    "500 Internal Server Error",
+    "502 Bad Gateway",
+    "521 Web Server Down"
+]
+export function obtenerEstado(){
+    return estado[Math.floor(Math.random() * 9)];
+}
+
