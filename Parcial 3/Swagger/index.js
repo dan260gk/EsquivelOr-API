785795b53762478d8a6dac5e7058a8679f449d0e
dan260gk <90198305+dan260gk@users.noreply.github.com>
2023-11-14 22:56:16 -0600

    s

--- Parcial 3/Swagger/index.js
+++ Parcial 3/Swagger/index.js
@@ -4,24 +4,44 @@ var fs = require('fs')
 var path = require('path')
 var mysql = require('mysql2')
 const swaggerUI=require('swagger-ui-express')
+const { SwaggerTheme } = require('swagger-themes');//
 const swaggerJsDoc=require('swagger-jsdoc')
-
+const redoc = require('redoc-express');
 
 var app = express()
+const theme = new SwaggerTheme('v3');
+
+const options = {
+  explorer: true,
+  customCss: theme.getBuffer('dark')
+};
+
+
+const def= fs.readFileSync(path.join(__dirname,'./swagger.json'),
+{encoding: 'utf8',flag:'r'})
+const read =fs.readFileSync(path.join(__dirname,'./README.MD'),
+{encoding: 'utf8',flag:'r'})
+const defObj=JSON.parse(def)
+defObj.info.description=read
 
 const swaggerOptions = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'API Usuarios',
-      version: '1.0.0',
-    },
-    servers: [
-      { url: "http://localhost:8081" }
-    ],
-  },
+  definition:defObj,
   apis: [`${path.join(__dirname, "./index.js")}`],
-};
+}
+
+// const swaggerOptions = {
+//   definition: {
+//     openapi: '3.0.0',
+//     info: {
+//       title: 'API Usuarios',
+//       version: '1.0.0',
+//     },
+//     servers: [
+//       { url: "http://localhost:8081" }
+//     ],
+//   },
+//   apis: [`${path.join(__dirname, "./index.js")}`],
+// };
 
 
 app.use(cors())
@@ -111,8 +131,45 @@ app.put("/usuarios/:id", async(req,res)=>{
 
 var algo;
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use("/api-docs",swaggerUI.serve,swaggerUI.setup(swaggerDocs));
+app.use("/api-docs",swaggerUI.serve,swaggerUI.setup(swaggerDocs, options));
+app.use("/api-docs-json",(req,res)=>{
+  res.json(swaggerDocs);
+})
 
-app.listen(8081,(req,res)=>{
+app.get(
+  '/docs',
+  redoc({
+    title: 'API Docs',
+    specUrl: '/api-docs-json',
+    nonce: '', // <= it is optional,we can omit this key and value
+    // we are now start supporting the redocOptions object
+    // you can omit the options object if you don't need it
+    // https://redocly.com/docs/api-reference-docs/configuration/functionality/
+    redocOptions: {
+      theme: {
+        colors: {
+          primary: {
+            main: '#6EC5AB'
+          }
+        },
+        typography: {
+          fontFamily: `"museo-sans", 'Helvetica Neue', Helvetica, Arial, sans-serif`,
+          fontSize: '15px',
+          lineHeight: '1.5',
+          code: {
+            code: '#87E8C7',
+            backgroundColor: '#4D4D4E'
+          }
+        },
+        menu: {
+          backgroundColor: '#ffffff'
+        }
+      }
+    }
+  })
+);
+
+
+app.listen(8082,(req,res)=>{
     console.log("servidor express  escuchando")
 })
